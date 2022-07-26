4dfd96765c90f887f362c42dda33884a090297dd
GinMu <gin.musheng@gmail.com>
2022-07-26 11:11:41 +0800

    init project

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  rules: {
+    "@typescript-eslint/no-unused-vars": "error",
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-empty-interface": "off",
+    "@typescript-eslint/no-var-requires": "off"
+  },
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"]
+};
