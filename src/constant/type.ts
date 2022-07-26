87c158b893623c815c9b71172925b5c2dcc3b0e9
onewirter <103101527+onewirter@users.noreply.github.com>
2022-07-26 13:55:39 +0800

    define constant values (#1)

---
+++ src/constant/type.ts
@@ -0,0 +1,23 @@
+export enum OperationType {
+  PAYMENT = "Payment",
+  ACCOUNT_SET = "AccountSet",
+  SIGNER_LIST_SET = "SignerListSet"
+}
+
+export enum ActionType {
+  REGISTER = "register",
+  UNREGISTER = "unregister",
+  MULTI_SIGN = "multiSign"
+}
+
+export enum MEMO_TYPE {
+  NAME_SERVICE = "name-service",
+  MULTI_SIGN = "multi-sign",
+  ORACLE = "oracle",
+  PAYLOAD = "payload",
+  TEMPLATE = "template"
+}
+
+export enum CHAIN_ID {
+  SWTC = "0x8000013b"
+}
