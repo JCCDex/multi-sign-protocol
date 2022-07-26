1ff7dc6e88687d1318debaf1ca44b94e1b1294c4
onewirter <1316788824@qq.com>
2022-07-26 13:34:24 +0800

    defind some const

---
+++ src/constant/type.ts
@@ -0,0 +1,19 @@
+export enum TransactionType {
+  PAYMENT = "Payment",
+  ACCOUNT_SET = "AccountSet",
+  SIGNER_LIST_SET = "SignerListSet",
+  REGISTER = "register",
+  UNREGISTER = "unregister"
+}
+
+export const TEMPLATE = "template";
+
+export const MULTI_SIGN = "multiSign";
+
+export enum TOPIC_TYPE {
+  NAME_SERVICE = "name-service",
+  MULTI_SIGN = "multi-sign",
+  ORACLE = "oracle"
+}
+
+export const PAYLOAD = "payload";
